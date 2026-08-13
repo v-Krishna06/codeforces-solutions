c903c75ee91898072e224be20ddb558ce8de6fa6
Krishna Verma <kv657677@gmail.com>
2026-08-13 23:43:48 +0530

    Add Divisibility Problem [A] from Codeforces

---
+++ 1328/A - Divisibility Problem/solution.cpp
@@ -0,0 +1,15 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+ 
+    int main() {
+        int t;
+        cin>>t;
+        while(t--){
+            int a,b;
+            cin>>a>>b;
+            int ans = 0;
+           cout << (b - a % b) % b << '
+';
+        }
+        return 0;
+    }
