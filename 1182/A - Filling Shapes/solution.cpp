8b2e6ba3dee0a54ec9105f61c3283dc15250fc20
Krishna Verma <kv657677@gmail.com>
2026-08-26 22:13:58 +0530

    Add Filling Shapes [A] from Codeforces

---
+++ 1182/A - Filling Shapes/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==1){
+        cout<< 0;
+    }
+    else{
+        cout << (1LL << (n / 2));
+    }
+ 
+ 
+    return 0;
+}
